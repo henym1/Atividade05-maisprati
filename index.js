7037a21e1c3c4e7489f503092c5fefaeb24bb0d1
Henry Adam <iihenymii@gmail.com>
2024-11-01 17:39:53 -0300

    adicionado botao editar

--- index.js
+++ index.js
@@ -48,14 +48,23 @@ function createTodoItem(todo, todoIndex) {
     <label class="todo-text" for="${todoId}">
         ${todoText}
     </label>
+    <button class="edit-button">
+        <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+    </button>
     <button class="delete-button">
         <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
     </button>
     `
+    const editButton = todoLi.querySelector(".edit-button")
+    editButton.addEventListener("click", () => {
+        editTodoItem(todoIndex)
+    })
+
     const deleteButton = todoLi.querySelector(".delete-button")
     deleteButton.addEventListener("click", () => {
         deleteTodoItem(todoIndex)
     })
+
     const checkbox = todoLi.querySelector("input")
     checkbox.addEventListener("change", () => {
         allTodos[todoIndex].completed = checkbox.checked
@@ -64,6 +73,15 @@ function createTodoItem(todo, todoIndex) {
 
     checkbox.checked = todo.completed
     return todoLi
+}
+
+function editTodoItem(todoIndex) {
+    const newTodoText = prompt("Edite a tarefa: ", allTodos[todoIndex].text)
+    if (newTodoText !== null && newTodoText.trim() !== "") {
+        allTodos[todoIndex].text = newTodoText.trim()
+        saveTodos()
+        updateTodoList()
+    }
 }
 
 function deleteTodoItem(todoIndex) {
